cfdaf9cc49938f2b0b29c501360e91a225511e00
Sara CHAYGANI <sara.chaygani.dev@gmail.com>
2024-11-20 22:12:35 +0700

    tested the formatedCurrentDateTime function.

--- dev/date-info.js
+++
@@ -1,4 +0,0 @@
-flatpickr("#current-date", {
-    dateFormat: "Y-m-d",  // Set the desired format
-    allowInput: true      // Allow users to input date manually
-});
---
+++ dev/dateTime-Info.js
@@ -0,0 +1,4 @@
+import { currentDatetime } from "../untility/untilities.js";
+
+document.querySelector('.current-date-time').value = currentDatetime();
+console.log(currentDatetime());
